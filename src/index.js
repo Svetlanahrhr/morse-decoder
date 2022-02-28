d2dd9df30af02733bcf92002fc79c1d0995159bf
Svetlana <kovalenkohp@mail.ru>
2022-02-28 15:43:43 +0400

    add: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	let arrExpr = expr.split('')
+	let letterDashDot =  ''
+	let resStr = []
+	console.log(expr.length);
+	for (let i = 0; i< expr.length; i+= 10) {
+		let letter = arrExpr.splice(0,10)
+		console.log(letter);
+		if (letter.join('') === '**********') {
+			resStr.push(' ')
+		}
+	
+		for (let j = 0; j< 5; j++) {
+			let symbols = letter.splice(0,2)
+			// console.log(symbols);
+			if (symbols.join('') === '10') {
+				letterDashDot = letterDashDot + '.'
+			}
+			if (symbols.join('') === '11') {
+				letterDashDot = letterDashDot + '-'
+			}
+			
+		}
+		console.log(letterDashDot);
+		resStr.push(MORSE_TABLE[letterDashDot])
+		letterDashDot = ''
+	}	
+	
+	
+	
+	return resStr.join('')
 }
 
 module.exports = {
